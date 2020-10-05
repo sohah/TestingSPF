fe80b84f9f871a55ede6229db104e7bcb4c4a53d
sohah <husse200@umn.edu>
2020-10-05 08:41:08 -0500

    adding logs, test cases and spinfo for all benchmarks and the invarints for some of them

--- scripts/generate_invairanttcas_spliter.sh
+++ scripts/generate_invairanttcas_spliter.sh
@@ -3,8 +3,10 @@ mkdir ../traces_tcas
 #java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/git/jpf-symbc/lib/daikon/daikon.jar daikon.Chicory --dtrace-file=../traces_tcas/tcas.dtrace.gz tcas.TCAS_Daikon /home/soha/git/jpf-symbc/testCase/testCases_tcas.txt
 #java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/git/jpf-symbc/lib/daikon/daikon.jar daikon.Daikon ../traces_tcas/*.dtrace.gz --format java > ../daikonInv/daikon_invariants_tcas.txt
 
+#java -cp /home/soha/git/jpf-symbc/src/examples/tcas:/home/soha/daikon-5.8.4/daikon.jar daikon.tools.jtb.CreateSpinfo ../src/examples/tcas/SpfTCAS.java
 
-java -cp /home/soha/git/jpf-symbc/src/examples/tcas:/home/soha/daikon-5.8.4/daikon.jar daikon.tools.jtb.CreateSpinfo ../src/examples/tcas/TCAS_Output.java
+echo "finished spliting"
+java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/git/jpf-symbc/lib/daikon/daikon.jar daikon.Chicory --dtrace-file=../traces_tcas/tcas.dtrace.gz tcas.TCAS_Daikon /home/soha/git/jpf-symbc/testCase/testCases_tcas_steps2.txt
 
-java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/git/jpf-symbc/lib/daikon/daikon.jar daikon.Chicory --dtrace-file=../traces_tcas/tcas.dtrace.gz tcas.TCAS_Daikon /home/soha/git/jpf-symbc/testCase/testCases_tcas.txt
-java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/git/jpf-symbc/lib/daikon/daikon.jar daikon.Daikon ../src/examples/tcas/TCAS_Output.spinfo ../traces_tcas/*.dtrace.gz --format java > ../daikonInv/daikon_invariants_tcasplitInfo.txt
+echo "finish chicory starting collecting invariants."
+java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/git/jpf-symbc/lib/daikon/daikon.jar daikon.Daikon ../src/examples/tcas/SpfTCAS_custom.spinfo ../traces_tcas/*.dtrace.gz --format java > ../daikonInv/daikon_invariants_tcasplitInfo.txt
--- scripts/generate_invairantwbs_spliter.sh
+++ scripts/generate_invairantwbs_spliter.sh
@@ -11,8 +11,10 @@ mkdir ../traces_wbs
 
 
 
-java -cp /home/soha/git/jpf-symbc/src/examples/wbs:/home/soha/daikon-5.8.4/daikon.jar daikon.tools.jtb.CreateSpinfo ../src/examples/wbs/WBS.java
+#java -cp /home/soha/git/jpf-symbc/src/examples/wbs:/home/soha/daikon-5.8.4/daikon.jar daikon.tools.jtb.CreateSpinfo ../src/examples/wbs/WBS.java
 
-java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/daikon-5.8.4/daikon.jar daikon.Chicory --dtrace-file=../traces_wbs/wbs.dtrace.gz wbs.WBS_Daikon /home/soha/git/jpf-symbc/testCase/testCases_wbs.txt
+#echo "finished spliting"
+java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/daikon-5.8.4/daikon.jar daikon.Chicory --dtrace-file=../traces_wbs/wbs.dtrace.gz wbs.WBS_Daikon /home/soha/git/jpf-symbc/testCase/testCases_wbs_steps5.txt
 
-java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/daikon-5.8.4/daikon.jar daikon.Daikon ../traces_wbs/*.dtrace.gz  ../src/examples/wbs/WBS.spinfo --format java > ../daikonInv/daikon_invariants_wbs.txt
+echo "finish chicory starting collecting invariants."
+java -cp /home/soha/git/jpf-symbc/build/examples:/home/soha/daikon-5.8.4/daikon.jar daikon.Daikon ../traces_wbs/*.dtrace.gz  ../src/examples/wbs/WBS_custom.spinfo --format java > ../daikonInv/daikon_invariants_wbs.txt
